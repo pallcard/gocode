b5b83516c2e83b3070eb8920ac154ac02b325b75
liuke <18162327131@163.com>
2020-05-17 18:13:22 +0800

    web

---
+++ src/webapp/main.go
@@ -0,0 +1,15 @@
+package webapp
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "hello world!", r.URL.Path)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
